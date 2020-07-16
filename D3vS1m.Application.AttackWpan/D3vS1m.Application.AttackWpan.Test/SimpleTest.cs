dcdd969101d413481e3df46a93d2069a9fcd26b2
Kafle Sagar <nephopsagar@gmail.com>
2020-07-16 13:47:53 +0200

    Initialized unit test

---
+++ D3vS1m.Application.AttackWpan/D3vS1m.Application.AttackWpan.Test/SimpleTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D3vS1m.Application.AttackWpan.Test
+{
+    [TestClass]
+    public class SimpleTest : TestBase
+    {
+
+        [TestMethod]
+        public void SimpleSimilatorTest()
+        {
+            // arrange
+
+          
+            // assert
+            Assert.IsTrue(true,"yay");
+        }
+
+
+    }
+}
